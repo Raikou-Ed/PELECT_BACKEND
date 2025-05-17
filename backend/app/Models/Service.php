2d1d7162919d9a78eaef1c166f206eb498f03c1c
Edrian M. Arsua <155219165+Raikou-Ed@users.noreply.github.com>
2025-05-17 13:07:10 +0800

    Add files via upload

---
+++ backend/app/Models/Service.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+
+class Service extends Model
+{
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'user_id',
+        'is_active',
+    ];
+
+    // Relation: A service belongs to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
+
