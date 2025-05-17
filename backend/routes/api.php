680a6521d2155dfcd4a56caceb5dcbc9855dd092
Edrian M. Arsua <155219165+Raikou-Ed@users.noreply.github.com>
2025-05-17 13:10:11 +0800

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\RevenueController;
+
+
+
 
 Route::get('/users', [UserController::class, 'getAllUsers']);
 Route::post('/users', [UserController::class, 'createUsers']);
+Route::apiResource('services', ServiceController::class);
+Route::apiResource('reviews', ReviewController::class);
+Route::apiResource('revenues', RevenueController::class);
 ?>
