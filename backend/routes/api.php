03e939b81498b3847feda6f8f0329cd766bc75fb
Edrian M. Arsua <155219165+Raikou-Ed@users.noreply.github.com>
2025-05-18 09:44:19 +0800

    Update api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,8 +5,9 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\RevenueController;
-
-
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\BookingHistoryController;
 
 
 Route::get('/users', [UserController::class, 'getAllUsers']);
@@ -15,4 +16,7 @@ Route::post('/login', [UserController::class, 'login']);
 Route::apiResource('services', ServiceController::class);
 Route::apiResource('reviews', ReviewController::class);
 Route::apiResource('revenues', RevenueController::class);
+Route::apiResource('bookings', BookingController::class);
+Route::apiResource('appointments', AppointmentController::class);
+Route::apiResource('booking-histories', BookingHistoryController::class);
 ?>
